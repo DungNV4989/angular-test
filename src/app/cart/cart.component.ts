62d93407144ece604d0ddfea876c0aaa0aa4244e
DungNV4989 <dungnv4989@gmail.com>
2022-02-14 16:05:09 +0700

    5 files modified, 4 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,49 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
-  ngOnInit(): void {}
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.log(this.checkoutForm.value, 'Your order has been submitted');
+    this.checkoutForm.reset();
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChange(): void {
+    console.log('ngOnChange');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
 }
